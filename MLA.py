13555bfcbac09a9d94910ae0aea1336d10643d2c
Franco Perez <56205122+dev-lang@users.noreply.github.com>
2021-08-03 17:17:03 -0300

    primera version
    dump en memoria de site_id y seller_id
    api rest en endpoints de meli

---
+++ MLA.py
@@ -0,0 +1,57 @@
+# VERSION 0.1 ALPHA
+''' https://developers.mercadolibre.com.ar/es_ar/items-y-busquedas '''
+
+
+# requests y json son obligatorios para trabajar con endpoints de ws
+import requests
+import json
+from pprint import pprint as visualizar
+
+''' Recorrer todos los ítems publicados por el seller_id = 179571326 del
+ site_id = "MLA" '''
+
+# estructura esperada:
+# api_rest + site id + endpoint + seller_id
+
+escribirArchivoRaw = open("data_crudo.json", "w")
+api_rest = "https://api.mercadolibre.com/sites/"
+endpoint = "/search?seller_id="
+seller_id = int(179571326)
+site_id = "MLA"
+filtros = ['id', 'title', 'category_id', 'name']
+pedido = requests.get(api_rest + site_id + endpoint + str(seller_id))
+pedidoJson = pedido.json()
+
+# MELI ARG
+
+
+'''
+
+Generar un archivo de LOG que contenga los siguientes datos de
+cada ítem:
+a. "id" del ítem, "title" del item, "category_id" donde está
+publicado, "name" de la categoría.
+
+'''
+
+
+def RecorrerItems(seller_id, site_id):
+    global pedido
+    global pedidoJson
+    print("acá iria la funcion para:", seller_id, "\nen: ", site_id, "\nFormando: ")
+    print(api_rest + site_id + endpoint + str(seller_id))
+    pedido = requests.get(api_rest + site_id + endpoint + str(seller_id))
+    #visualizar(pedido.json()
+    #visualizar(pedidoJson)
+    visualizar(pedidoJson)
+
+    
+
+RecorrerItems(seller_id, site_id)
+
+
+
+
+
+
+
